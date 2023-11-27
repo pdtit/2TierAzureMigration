0df7454690752d14d5b1904cdd0a1f10cc5439bd
Peter De Tender <peter@pdtit.be>
2023-11-27 07:47:47 -0800

    Update configurevm.ps1

--- JumpVM/configurevm.ps1
+++ JumpVM/configurevm.ps1
@@ -1,5 +1,10 @@
-# Install Chocolatey Packager
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+# Install Chocolatey
+
+# Set Chocolatey version to 1.4.0 to avoid installing .NET 4.8 Framework (which the newer 2.0 needs, but requires reboot, and blocks CustomScriptExtension
+
+$env:chocolateyVersion = '1.4.0'
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # Silent Install Software Tools
 # Install System Tools
